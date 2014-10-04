5f3af49f50ac6892fedffd559cb8864c21be5586
caver <caver@caver-pc>
2014-10-04 16:36:03 +0700

    Fix conflict with other inventory modification plugins (like JSNONAPI) in offline mode. Double null check removed.

--- src/com/lishid/openinv/internal/v1_7_R1/SpecialPlayerInventory.java
+++ src/com/lishid/openinv/internal/v1_7_R1/SpecialPlayerInventory.java
@@ -1,15 +1,15 @@
 /*
  * Copyright (C) 2011-2014 lishid.  All rights reserved.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation,  version 3.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -50,7 +50,8 @@ public class SpecialPlayerInventory extends PlayerInventory implements ISpecialP
     @Override
     public void InventoryRemovalCheck() {
         owner.saveData();
-        if (transaction.isEmpty() && !playerOnline) {
+        // Conflict with other Offline inventory modification plugins like JSONAPI
+        if (transaction.isEmpty() || !playerOnline) {
             OpenInv.inventories.remove(owner.getName().toLowerCase());
         }
     }
