c8938f451d3f2fca1b21ed538d872dd2fc3ed4c0
Jikoo <jikoo.games@gmail.com>
2020-02-01 08:03:15 -0500

    Update copyright

--- plugin/src/main/java/com/lishid/openinv/util/ConfigUpdater.java
+++ plugin/src/main/java/com/lishid/openinv/util/ConfigUpdater.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011-2019 lishid. All rights reserved.
+ * Copyright (C) 2011-2020 lishid. All rights reserved.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
