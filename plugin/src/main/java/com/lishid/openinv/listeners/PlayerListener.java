331fae8c0ea3ee4688ed1964c95ae6e3846909cd
mfnalex <1122571+mfnalex@users.noreply.github.com>
2020-07-16 03:56:30 +0200

    Add support for InvUnload

--- plugin/src/main/java/com/lishid/openinv/listeners/PlayerListener.java
+++ plugin/src/main/java/com/lishid/openinv/listeners/PlayerListener.java
@@ -54,6 +54,12 @@ public class PlayerListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPlayerInteract(PlayerInteractEvent event) {
+
+        // Do not cancel the event if it is the InvUnloadCheckAccessEvent
+        if(!event.getClass().getName().equals("de.jeff_media.InvUnload.API.InvUnloadCheckAccessEvent")) {
+            return;
+        }
+
         if (event.getAction() != Action.RIGHT_CLICK_BLOCK || event.getPlayer().isSneaking()
                 || event.useInteractedBlock() == Result.DENY || event.getClickedBlock() == null
                 || !plugin.getAnySilentContainer().isAnySilentContainer(event.getClickedBlock())) {
