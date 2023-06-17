922586c92532ccfcda41c974fd8bbf10607b0f8c
XPYEX <50171612+0XPYEX0@users.noreply.github.com>
2023-06-17 19:35:21 +0800

    Improve support for non-UTF-8 systems (#151)

--- plugin/src/main/java/com/lishid/openinv/util/lang/LanguageManager.java
+++ plugin/src/main/java/com/lishid/openinv/util/lang/LanguageManager.java
@@ -22,6 +22,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -89,7 +90,7 @@ public class LanguageManager {
         if (resourceStream == null) {
             localeConfigDefaults = new YamlConfiguration();
         } else {
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(resourceStream))) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(resourceStream, StandardCharsets.UTF_8))) {
                 localeConfigDefaults = YamlConfiguration.loadConfiguration(reader);
             } catch (IOException e) {
                 plugin.getLogger().log(Level.WARNING, e, () -> "[LanguageManager] Unable to load resource " + locale + ".yml");
